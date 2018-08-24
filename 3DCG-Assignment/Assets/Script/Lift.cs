fdbf7c818af8a162b59bcff80fd2262fd9623742
ChinJL <33471703+ChinJL@users.noreply.github.com>
2018-08-24 16:36:55 +0800

    - Building - Lift

---
+++ 3DCG-Assignment/Assets/Script/Lift.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour {
+
+	private bool isEvevatorUp = false;
+	private Animator anim;
+
+	public void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	public void Update()
+	{
+		if (!isEvevatorUp)
+		{
+			anim.SetBool ("isRunningUp", true);
+			isEvevatorUp = true;
+		} 
+//		else
+//		{
+//			anim.SetBool ("isRunningUp", false);
+//			isEvevatorUp = false;
+//		}
+	}
+}
